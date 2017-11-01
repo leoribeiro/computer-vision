c8606df84e842389bc99e57a4c3cb3a3ad343059
Leonardo Ribeiro <leonardoribeiro@iMac-Leo.local>
2017-11-01 21:50:40 -0200

    rensac for multiple images

--- ex3/ex3_ransac.py
+++ ex3/ex3_ransac.py
@@ -21,18 +21,11 @@ import cv2
 import matplotlib
 import random
 import sys
-from scipy.spatial import distance
 
 ######### RANSAC
 
-kp1 = []
-kp2 = []
-matches = []
 path_images = {}
-coords = {}
-images = {}
-images_ = {}
-cont_images = 0
+putative_correspondences = []
 
 
 def center(toplevel):
@@ -48,7 +41,7 @@ def center(toplevel):
 def norm_x(x):
   if(x[2] == 0):
     return x
-  return [x[0]/x[2],x[1]/x[2],x[2]/x[2]]
+  return np.array([x[0]/x[2],x[1]/x[2],x[2]/x[2]])
 
 def loadImage(image,title):
    global fig_a
@@ -61,46 +54,69 @@ def loadImage(image,title):
    plt.show()
    return
 
-def generateNewImage(h,h_inv):
+def get_points_corners(h,img):
+    x = np.dot(h,[0,0,1])
+    x1 = norm_x(x)
 
-  img1 = Image.open(path_images[1])
-  img2 = Image.open(path_images[2])
+    x = np.dot(h,[0,img.size[1] - 1,1])
+    x2 = norm_x(x)
+
+    x = np.dot(h,[img.size[0] - 1,img.size[1] - 1,1])
+    x3 = norm_x(x)
+
+    x = np.dot(h,[img.size[0] - 1,0,1])
+    x4 = norm_x(x)
+
+    return x1,x2,x3,x4
+
+def convert_points(h,points):
+  xs = []
+  for p in points:
+    x = np.dot(h,p)
+    x = norm_x(x)
+    xs.append(x)
+  return xs
+
+def generateNewImage(hs,filenames):
+
+  middleIndex = int((len(filenames) - 1)/2)
+  print ("middleIndex",middleIndex)
   
-  #pixels = image.load() # create the pixel map
-  width0, height0 = img1.size
-  width1, height1 = img2.size
+  width = 0
+  img = []
+  for k,f in filenames.items():
+    img_ = Image.open(f)
+    img.append(img_)
+    width += img_.size[0]
+    height = img_.size[1]
 
-  height = height0
-  width = width0 + width1
+  xs_l = []
 
+  for plano in range(0,middleIndex):
+    h = hs[plano]
+    x1,x2,x3,x4 = get_points_corners(h,img[plano])
+    xs_l = convert_points(h,xs_l)
+    xs_l.extend([x1,x2,x3,x4])
+
+  xs_r = []
+  for plano in range(len(hs)-1,middleIndex-1,-1):
+    h_inv = np.linalg.inv(hs[plano])
+    x1,x2,x3,x4 = get_points_corners(h_inv,img[plano])
+    xs_r = convert_points(h_inv,xs_r)
+    xs_r.extend([x1,x2,x3,x4])
+
+  xs_ = xs_r + xs_l
   xs = []
   ys = []
-
-  x = np.dot(h_inv,[0,0,1])
-  x = norm_x(x)
-  xs.append(x[0])
-  ys.append(x[1])
-
-
-  x = np.dot(h_inv,[0,height1 - 1,1])
-  x = norm_x(x)
-  xs.append(x[0])
-  ys.append(x[1])
-
-  x = np.dot(h_inv,[width1 - 1,height1 - 1,1])
-  x = norm_x(x)
-  xs.append(x[0])
-  ys.append(x[1])
-
-  x = np.dot(h_inv,[width1 - 1,0,1])
-  x = norm_x(x)
-  xs.append(x[0])
-  ys.append(x[1])
+  for x in xs_:
+    xs.append(x[0])
+    ys.append(x[1])
+    
 
   xs.append(0)
-  xs.append(width0 - 1)
+  xs.append(img[middleIndex].size[0] - 1)
   ys.append(0)
-  ys.append(height0 - 1)
+  ys.append(img[middleIndex].size[1] - 1)
 
   min_x = min(xs)
   min_y = min(ys)
@@ -120,35 +136,45 @@ def generateNewImage(h,h_inv):
   step_y = (max_y - min_y)/n_height
   step_x = (max_x - min_x)/n_width
   x_cm = min_x
-  print ("width",width)
-  print ("height",height)
+  #print ("width",width)
+  #print ("height",height)
   for x in range(n_width):
     y_cm = min_y
     for y in range(n_height):
-      coords = np.dot(h,[x_cm,y_cm,1])
-      coords = norm_x(coords)
-      try:
-        new_pixel = img1.getpixel((x_cm,y_cm))
-        new_image.putpixel((x,y),new_pixel)
-      except IndexError:
-        pass
-      try:
-        new_pixel = img2.getpixel((coords[0],coords[1]))
-        new_image.putpixel((x,y),new_pixel)
-      except IndexError:
-        pass
+      for k in range(0,len(hs)+1):
+        if(k == middleIndex):
+          put_pixel(x_cm,y_cm,x,y,img[k],new_image)
+        else:
+          h = apply_matrix_recursive(k,middleIndex,hs)
+          coords = np.dot(h,[x_cm,y_cm,1])
+          coords = norm_x(coords)
+          put_pixel(coords[0],coords[1],x,y,img[k],new_image)
       y_cm += step_y
     x_cm += step_x
 
   return new_image
 
-def applyMatrix(h,h_inv):
+def apply_matrix_recursive(k,m,hs):
+  if(k > m):
+    h_ = hs[k-1]
+    for h in range(k-2,m-1,-1):
+      h_ = np.dot(h_,hs[h])
+  else:
+    h_ = np.linalg.inv(hs[k])
+    for h in range(k+1,m):
+      h_inv = np.linalg.inv(hs[h])
+      h_ = np.dot(h_,h_inv)
+  return h_
 
-  print ("gerando imagem...")
-  new_image = generateNewImage(h,h_inv)
-  print ("imagem gerada.")
-  return new_image
 
+
+
+def put_pixel(x_,y_,x,y,image,new_image):
+  try:
+    new_pixel = image.getpixel((x_,y_))
+    new_image.putpixel((x,y),new_pixel)
+  except IndexError:
+    pass
 
 def drawMatches(img1, kp1, img2, kp2, matches):
 
@@ -172,38 +198,60 @@ def drawMatches(img1, kp1, img2, kp2, matches):
 
     return out
 
-def compare(filename1, filename2):
-    global matches
-    global kp1,kp2
+def normalizeMatches(matches):
+  matches = sorted(matches, key=lambda val: val.distance)
+  matches_ = []
+  for m in matches:
+    if(m.distance < 250):
+      matches_.append(m)
+  return matches_
 
-    img1 = cv2.imread(filename1)
-    img2 = cv2.imread(filename2)
 
+def compare(filenames):
     # Initiate SIFT detector
     sift = cv2.xfeatures2d.SIFT_create()
 
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+    kp = []
+    des = []
+    img = []
+    for k,f in filenames.items():
+      img_ = cv2.imread(f)
+      img.append(img_)
+      # find the keypoints and descriptors with SIFT
+      kp_, des_ = sift.detectAndCompute(img_,None)
+      kp.append(kp_)
+      des.append(des_)
 
     # BFMatcher with default params
     bf = cv2.BFMatcher()
-    matches = bf.match(des1,des2)
 
-    matches_o = sorted(matches, key=lambda val: val.distance)
+    matches = []
+    for n,d in enumerate(des):
+      try:
+        matches_ = bf.match(des[n],des[n+1])
+        matches_ = normalizeMatches(matches_)
+        matches.append(matches_)
+      except IndexError:
+        pass
 
-    #print (len(matches))
 
-    img3 = drawMatches(img1,kp1,img2,kp2,matches_o[:25])
-    img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
-    fig, ax = plt.subplots(num=None, figsize=(16, 6), dpi=80, facecolor='w', edgecolor='k')
-    fig.subplots_adjust(bottom = 0)
-    fig.subplots_adjust(top = 1)
-    fig.subplots_adjust(right = 1)
-    fig.subplots_adjust(left = 0)
-    plt.imshow(img3) 
-    plt.ion()
-    plt.show()
+    get_putative_correspondences(matches,kp)
+
+    # for m in matches_[-25:]:
+    #  print (m.distance)
+
+    # print ("Matches < 200:",len(matches_))
+
+    # img3 = drawMatches(img1,kp1,img2,kp2,matches_[-25:])
+    # img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+    # fig, ax = plt.subplots(num=None, figsize=(16, 6), dpi=80, facecolor='w', edgecolor='k')
+    # fig.subplots_adjust(bottom = 0)
+    # fig.subplots_adjust(top = 1)
+    # fig.subplots_adjust(right = 1)
+    # fig.subplots_adjust(left = 0)
+    # plt.imshow(img3) 
+    # plt.ion()
+    # plt.show()
 
     return
 
@@ -213,48 +261,55 @@ def returnMatrix(x,x_):
   #line3 = [-x_[1]*x[0],-x_[1]*x[1],-x_[1]*x[2],x_[0]*x[0],x_[0]*x[1],x_[0]*x[2],0,0,0]
   return line1,line2
 
-def get_poits_random(num_points):
-  global coords
-  coords[0] = []
-  coords[1] = []
+def get_poits_random(num_points,correspondences):
+  coords = []
   cont = 0
 
   while cont < num_points:
-    m = random.choice(matches)
-    img1_idx = m.queryIdx
-    img2_idx = m.trainIdx
-    (x1,y1) = kp1[img1_idx].pt
-    (x2,y2) = kp2[img2_idx].pt
-    coords[0].append((x1,y1))
-    coords[1].append((x2,y2))
+    m = random.choice(correspondences)
+    (x1,y1) = m[0]
+    (x2,y2) = m[1]
+    coords.append([(x1,y1),(x2,y2)])
     cont += 1
 
-def calc_matrix():
-  get_poits_random(4)
+  return coords
+
+def get_putative_correspondences(matches,kp):
+  global putative_correspondences
+
+  cont = 0
+  cont_k = 0
+  for m_ in matches:
+    putative_correspondences.append([])
+    for m in m_:
+      img1_idx = m.queryIdx
+      img2_idx = m.trainIdx
+      (x1,y1) = kp[cont][img1_idx].pt
+      (x2,y2) = kp[cont+1][img2_idx].pt
+      putative_correspondences[cont].append([(x1,y1),(x2,y2)])
+    cont += 1
+
+def calc_matrix(correspondences):
+  N = get_num_poits()
+  coords = get_poits_random(N,correspondences)
   a = [] 
-  for i in range(0,4):
-    A = returnMatrix([coords[0][i][0],coords[0][i][1],1],[coords[1][i][0],coords[1][i][1],1])
+  for c in coords:
+    A = returnMatrix([c[0][0],c[0][1],1],[c[1][0],c[1][1],1])
     a.append(A[0])
     a.append(A[1])
   a = np.array(a)
 
   U, s, V = np.linalg.svd(a, full_matrices=True)
-  #print ("U",U)
-  #print ("s",s)
-  #print ("V",V)
 
   c = V[-1]
-  #print ("c",c)
   h = np.array([[c[0],c[1],c[2]],[c[3],c[4],c[5]],[c[6],c[7],c[8]]])
-  #print ("h",h)
   return h
 
 def symmetric_transfer_error(x,x_,h,h_inv):
-  d1 = np.power(distance.euclidean(x,norm_x(np.dot(h_inv,x_))),2)
-  #print (x,norm_x(np.dot(h_inv,x_)))
-  d2 = np.power(distance.euclidean(x_,norm_x(np.dot(h,x))),2)
-  #print (x_,norm_x(np.dot(h,x)))
-  #print ("-")
+  x = np.array(x)
+  x_ = np.array(x_)
+  d1 = np.linalg.norm(x-norm_x(np.dot(h_inv,x_)))
+  d2 = np.linalg.norm(x_-norm_x(np.dot(h,x)))
   return d1 + d2
 
 def get_num_poits():
@@ -263,24 +318,18 @@ def get_num_poits():
 def get_threshold():
   return int(e2.get())
 
-def get_iterations():
-  return int(e1.get())
 
-
-def get_inliers(h,num_points):
+def get_inliers(h,correspondences):
   inliers = 0
   h_inv = np.linalg.inv(h)
-  #print (coords)
-  get_poits_random(num_points)
-  for i in range(0,num_points):
-    x = [coords[0][i][0],coords[0][i][1],1]
-    x_ = [coords[1][i][0],coords[1][i][1],1]
+  threshold = get_threshold()
+  for c in correspondences:
+    x = [c[0][0],c[0][1],1]
+    x_ = [c[1][0],c[1][1],1]
     d = symmetric_transfer_error(x,x_,h,h_inv)
-    #print ("d",d)
     
-    if d < get_threshold():
+    if d < threshold:
       inliers += 1
-  #print ("--")
 
   return inliers
 
@@ -289,62 +338,59 @@ def get_n_samples(s,e,p):
   return np.log(1.0-p)/np.log(1-np.power((1-e),s))
 
 
-def adaptative_number_samples():
-
-  s = get_num_poits()
-  global matches
-  total_points = len(matches)
-
-  e = 0.2
-
-  sample_count = 0
-  N = 999999999
-  while N > sample_count:
-    h = calc_matrix()
-    T = int((1 - e)*total_points)
-    print ("T",T)
-    inliers = get_inliers(h,T)
-    print ("inliers",inliers)
-    e = 1 - inliers/total_points
-    N = get_n_samples(s,e,0.99)
-
-  print (N)
-  return int(N)
-
-
-
-
-def rensac():
+def rensac(correspondences):
+  
+  total_points = len(correspondences)
+  s = 4
   print ("Executing adaptative_number_samples...")
-  N = adaptative_number_samples()
+  #N = adaptative_number_samples()
+  N = 99999999
+  sample_count = 0
   print ("Number of samples:",N)
   print ("starting RENSAC...")
   best = 0
-  H = calc_matrix()
-  for n in range(0,N):
-    #print ("Executing ",n+1,"iteration...")
-    h = calc_matrix()
-    inliers = get_inliers(h,get_num_poits())
+  e = 1
+  while N > sample_count:
+    print ("Executing ",sample_count+1,"iteration...")
+    print ( "Calculando matriz...")
+    h = calc_matrix(correspondences)
+    print ("Calculada")
+    print ("calculando inliear")
+    inliers = get_inliers(h,correspondences)
+    e_ = 1 - (inliers*1.0)/total_points
+    if(e_ < e):
+      e = e_
+      N = get_n_samples(s,e,0.99)
+    print ("inliers:",inliers)
+    print ("e",e)
+    print ("N",N)
+    
+    print ("calculados")
     if(inliers > best):
-      print ("best number of inliers:",inliers)
+      #print ("best number of inliers:",inliers)
       H = h
       best = inliers
-    #print (n+1,"executed.")
+    #print (sample_count+1,"executed.")
+    print ("-")
+    sample_count += 1
   print ("Final best:",best)
   return H
 
 
 
-
 def compareImages():
-  print (path_images)
-  compare(path_images[1],path_images[2])
+  print ("Imagens:",path_images)
+  compare(path_images)
   return
 
 def generateImage():
-  h = rensac()
-  h_inv = np.linalg.inv(h)
-  new_image = applyMatrix(h,h_inv)
+  hs = []
+  for correspondences in putative_correspondences:
+    h = rensac(correspondences)
+    hs.append(h)
+  print ("gerando imagem...")
+  new_image = generateNewImage(hs,path_images)
+  print ("imagem gerada.")
   loadImage(new_image,'Panorama')
   return 
 
@@ -352,30 +398,29 @@ window.title("Image transformation")
 window.geometry("500x500")
 center(window)
 
-img1 = sys.argv[1]
-img2 = sys.argv[2]
-path_images[1] = img1
-path_images[2] = img2
 
-C = tk.Button(window, text ="Exec sift", command = compareImages)
-C.grid(row=1)
+for n,f in enumerate(sys.argv[1:]):
+  path_images[n] = f
 
-tk.Label(window, text="Iterations").grid(row=3, column=0)
+compareImages()
+
+# C = tk.Button(window, text ="Exec sift", command = compareImages)
+# C.grid(row=1)
+
 tk.Label(window, text="Threshold").grid(row=4, column=0)
 tk.Label(window, text="Points").grid(row=5, column=0)
 
-e1 = tk.Entry(window)
-e1.insert(tk.END, '1000')
 e2 = tk.Entry(window)
 e2.insert(tk.END, '5')
 e3 = tk.Entry(window)
-e3.insert(tk.END, '5')
-e1.grid(row=3, column=1)
+e3.insert(tk.END, '4')
 e2.grid(row=4, column=1)
 e3.grid(row=5, column=1)
 
 C = tk.Button(window, text ="Rensac and panoramic", command = generateImage)
 C.grid(row=6)
+
+
 
 
 window.mainloop()
